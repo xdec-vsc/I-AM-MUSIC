85bfe6d934c8fe275de19cc973f1ef54a508bc71
xdec <145052411+xdec-vsc@users.noreply.github.com>
2025-05-22 08:55:06 +0300

    hate by jpgmafia

---
+++ odevYÜZ değil ama 9.1/odev100 değil ama 9.1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace odev100_değil_ama_9._1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
